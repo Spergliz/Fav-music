6027cbf336269e387d374c179498cc95e52b2ec6
Lizzy Taylor Stromsmoe <p.Stromsmoe@gmail.com>
2022-12-13 14:45:39 -0700

    <

---
+++ main.js
@@ -0,0 +1,15 @@
+let music =[]
+
+// html elements
+let musicnameE1 = document.getElementById("Songname")
+let artistE1 = document.getElementById("Artist")
+let lengthE1= document.getElementById("Length of song")
+let btnE1= document.getElementById("btn")
+
+btnE1.addEventListener("click", btnhandle)
+
+function displayMusic () {}
+
+function addMusic() {} 
+
+function btnhandle() {}
