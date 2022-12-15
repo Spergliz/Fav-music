6564c321190e253ad923cf176bcd7890379685ec
Lizzy Taylor Stromsmoe <p.Stromsmoe@gmail.com>
2022-12-15 14:42:53 -0700

    vds

--- main.js
+++ main.js
@@ -1,47 +1,59 @@
-let music =[]
+let music = loadmusic();
 
 // html elements
-let musicdivE1 = document.getElementById("Music")
-let musicnameE1 = document.getElementById("Songname")
-let artistE1 = document.getElementById("Artist")
-let lengthE1= document.getElementById("Length of song")
-let btnE1= document.getElementById("btn")
+let musicdivE1 = document.getElementById("Music");
+let musicnameE1 = document.getElementById("Songname");
+let artistE1 = document.getElementById("Artist");
+let lengthE1 = document.getElementById("Length of song");
+let songcoverE1 = document.getElementById("song cover");
+let btnE1 = document.getElementById("btn");
 
-btnE1.addEventListener("click", btnhandle)
+btnE1.addEventListener("click", addMusic);
 
-function displayMusic () { let outputstr = ``;
-for (let i = 0; i < music.length; i++) {
-  outputstr += getmusicStr(music[i], i);
-}
-musicdivE1.innerHTML = outputstr;
+function displayMusic() {
+  let outputstr = ``;
+  for (let i = 0; i < music.length; i++) {
+    outputstr += getmusicStr(music[i], i);
+  }
+  musicdivE1.innerHTML = outputstr;
 }
 
 function addMusic() {
-music.push(newsong(musicnameE1,artistE1,lengthE1))
-displayMusic(); 
-} 
-
-function removehandle(){}
-
-function newsong (Song,Artist,length){
-return{ 
-song: Song,
-artist:Artist,
-legnth: length,
+  music.push(
+    newsong(
+      musicnameE1.value,
+      artistE1.value,
+      lengthE1.value,
+      songcoverE1.value
+    )
+  );
+  displayMusic();
+  savemusic();
 }
+
+function removehandle() {}
+
+function newsong(Song, Artist, length, songcover) {
+  return {
+    song: Song,
+    artist: Artist,
+    legnth: length,
+    SC: songcover,
+  };
 }
 function savemusic() {
-    localStorage.setItem("music", JSON.stringify(music));
-  }
-  
-  function loadcontacts() {
-    let musicstr = localStorage.getItem("music");
-    return JSON.parse(musicstr) ?? [];
-  }
-  function getmusicStr(music, i) {
-    return `
+  localStorage.setItem("music", JSON.stringify(music));
+}
+
+function loadmusic() {
+  let musicstr = localStorage.getItem("music");
+  return JSON.parse(musicstr) ?? [];
+}
+function getmusicStr(music, i) {
+  return `
     <div> 
-   
+    Favourite  Song ${i} <br>
+    <img src="${music.SC}">
     </div>
     `;
-  }
+}
